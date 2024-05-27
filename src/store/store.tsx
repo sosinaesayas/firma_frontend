11d8801ddc7995e6736b030f1c5cb55926c9377d
Yosinan <ymymym4634@gmail.com>
2024-05-27 23:28:15 +0300

    i've added formReducer for the company signup at line 16

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -4,6 +4,8 @@ import themeConfigReducer from './theme_config_slice';
 import tenderTableReducer from '../features/get_tenders/tender_table_slice';
 import formTenderReducer from "../features/create_tenders/components/tender_forms/tender_forms_slice";
 import productsReducer from "../features/create_tenders/components/products_form/products_form_slice";
+import formReducer from "../features/auth/company/company_auth_slice";
+
 const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -11,6 +13,7 @@ const store = configureStore({
     tenderTable: tenderTableReducer,
     formTender: formTenderReducer,
     products: productsReducer,
+    form: formReducer,
   },
 });
 
