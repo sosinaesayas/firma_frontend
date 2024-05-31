9d3a208c09700e2179406b4f92e01b8d720c324e
Yosinan <ymymym4634@gmail.com>
2024-05-31 23:22:26 +0300

    included routes for company signup and otp verification

---
+++ push.sh
@@ -0,0 +1,2 @@
+git commit -m "included the redirection to verify OTP page after successful signup" 
+git push origin signup
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,37 +1,32 @@
-import { useRoutes } from 'react-router-dom';
+import { useRoutes } from "react-router-dom";
 import HomePage from "../pages/home_page";
 import LoginPage from "../pages/login";
 import SignupPage from "../pages/signup";
 import Tenders from "../pages/admin/tenders";
 import CreateTender from "../pages/admin/create_tender";
-import PublicTender from '../pages/public_tender';
+import PublicTender from "../pages/public_tender";
 import Product from "../pages/products_trial";
-import SignUp from '../pages/company/SignUp';
-import OTP from '../pages/company/OTP';
-
+import SignUp from "../pages/company/SignUp";
+import OTP from "../pages/company/OTP";
 const RoutesConfig: React.FC = () => {
   const routes = useRoutes([
-    { 
-      path: "/", 
-      element: <HomePage />, 
-      children: [
-        { path: '', element: <Tenders/> }
-      ]
-  },
+    {
+      path: "/",
+      element: <HomePage />,
+      children: [{ path: "", element: <Tenders /> }],
+    },
 
     { path: "/login", element: <LoginPage /> },
     { path: "/signup", element: <SignupPage /> },
     { path: "/company/signup", element: <SignUp /> },
-    { path: "/otp-verification", element: <OTP />},
-   
-    { 
-      path: "create_tender", 
-      element: <PublicTender/>, 
-      children: [
-        { path: '', element: <CreateTender/> }
-      ]
-  },
-  {path: "/products", element:<Product/> },
+    { path: "/verify-otp", element: <OTP />},
+
+    {
+      path: "create_tender",
+      element: <PublicTender />,
+      children: [{ path: "", element: <CreateTender /> }],
+    },
+    { path: "/products", element: <Product /> },
   ]);
 
   return routes;
