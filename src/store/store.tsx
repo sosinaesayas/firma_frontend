173c9ba24d2c315e4bb9bdb0545eeef4017746c2
Yosinan <ymymym4634@gmail.com>
2024-05-31 23:25:35 +0300

    included formReducer and responseReducer for company signup

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/auth_slice";
-import themeConfigReducer from './theme_config_slice';
-import tenderTableReducer from '../features/get_tenders/tender_table_slice';
+import themeConfigReducer from "./theme_config_slice";
+import tenderTableReducer from "../features/get_tenders/tender_table_slice";
 import formTenderReducer from "../features/create_tenders/components/tender_forms/tender_forms_slice";
 import productsReducer from "../features/create_tenders/components/products_form/products_form_slice";
 import formReducer from "../features/auth/company/company_auth_slice";
@@ -21,7 +21,4 @@ const store = configureStore({
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 
-
 export default store;
-    
-
